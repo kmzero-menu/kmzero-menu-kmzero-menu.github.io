f11176f24515aa05cc13938d9838bd378b64196f
Gerardo Napoli <gerry.nap98@gmail.com>
2024-06-28 17:09:50 +0200

    Updated menu sections and menu items colors

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,9 +5,9 @@ import '../assets/styles/MenuItem.css';
 const MenuItem = ({ item }) => {
   return (
     <div className="menu-item">
-      <img src={item.image} alt={item.name} className="item-image" />
       <div className="item-details">
-        <h3>{item.name}</h3>
+        <h2>{item.name}</h2>
+        <p>{item.description}</p>
         <p>{item.price} €</p>
       </div>
     </div>
